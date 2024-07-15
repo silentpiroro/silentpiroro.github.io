797c7cdbf8728ad1bbe706b9e9c83d0936b46da0
Gsh <15170702455@163.com>
2024-07-15 10:26:13 +0800

    Initial deployment

---
+++ images/walttec/ai/rename_images.bat
@@ -0,0 +1,17 @@
+@echo off
+setlocal enabledelayedexpansion
+
+set "directory=E:\huaer\Nueink\walttec-org\prixite.github.io-master\prixite.github.io-master\public\images\walttec\ai"
+set "prefix=imgAI"
+set "counter=1"
+
+cd /d "%directory%"
+
+for %%f in (*.jpg *.png) do (
+    set "filename=%%~nf"
+    ren "%%f" "%prefix%!counter!.png"
+    set /a counter+=1
+)
+
+echo All files have been renamed and converted to PNG.
+pause
